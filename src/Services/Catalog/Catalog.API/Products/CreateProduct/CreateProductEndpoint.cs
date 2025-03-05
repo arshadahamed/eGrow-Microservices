bc97d6bd673993e6416466fc029ca486567c68b5
arshadahamed <arshadpayoneer@gmail.com>
2025-03-05 12:01:53 +0530

    Developed Create Product Endpoint with Minimal Apis and Carter

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,7 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
+public record class CreateProductRequest(string Name, List<string> Category, string Description, string ImgaeFile, decimal Price);
+public record CreateProductResponse(Guid Id);
 public class CreateProductEndpoint
 {
 }
