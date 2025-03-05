295f3a7757eeb8b5e752ecc111e0c6dd90da526a
arshadahamed <arshadpayoneer@gmail.com>
2025-03-05 12:10:12 +0530

    Developed POST Endpoint with Carter implements ICarterModule for Minimal Apis

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,12 +1,6 @@
-﻿global using Carter;
+﻿global using BuildingBlocks.CQRS;
+global using Carter;
+global using Catalog.API.Models;
+global using FluentValidation;
 global using Mapster;
 global using MediatR;
-global using Marten;
-global using BuildingBlocks.CQRS;
-global using Catalog.API.Models;
-global using Catalog.API.Exceptions;
-global using FluentValidation;
-global using BuildingBlocks.Behaviors;
-global using BuildingBlocks.Exceptions.Handler;
-global using Catalog.API.Data;
-global using Marten.Pagination;
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,7 +1,27 @@
-﻿namespace Catalog.API.Products.CreateProduct;
+﻿
+namespace Catalog.API.Products.CreateProduct;
 
 public record class CreateProductRequest(string Name, List<string> Category, string Description, string ImgaeFile, decimal Price);
 public record CreateProductResponse(Guid Id);
-public class CreateProductEndpoint
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request,ISender sender ) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create a new product")
+        .WithDescription("Create a new product in the catalog");
+
+    }
 }
